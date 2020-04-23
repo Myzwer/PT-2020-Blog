eea8e5ef0007e8075ae02a9b7c0607f7c0b6450a
Josh Forrester <twicearoundmars@yahoo.com>
2020-04-23 17:02:42 -0400

    added new section to consulting page

--- devwp/consulting.php
+++ devwp/consulting.php
@@ -14,7 +14,7 @@
 get_header(); ?>
 
     <div class="grid-container full-width">
-        <div class="grid-x grid-padding-x full-background full-background-xl" style = "background: url(http://pt-blog-2020.local/wp-content/uploads/2020/04/Consulting.jpg);  background-position: top center;background-repeat: no-repeat;background-attachment: scroll; background-size: cover;">
+        <div class="grid-x grid-padding-x full-background full-background-xl" style = "background: url(http://pt-blog-2020.local/wp-content/uploads/2020/04/Consulting-header.jpg);  background-position: top center;background-repeat: no-repeat;background-attachment: scroll; background-size: cover;">
             <div class="small-12 cell">
                 <div class="content-middle width-large">
                     <h1 class = "center" >Consulting</h1>
@@ -37,6 +37,64 @@ get_header(); ?>
         </div>
     </div>
 
+    <div class="full-width primary-background padding-bottom">
+        <div class = "grid-container padding-bottom padding-top">
+            <div class="grid-x grid-padding-x main-background rounded-corners">
+                <div class="small-12 cell padding-top">
+                    <h2 class = "center padding-bottom">// The Process</h2>
+                </div>
+
+                <div class = "grid-container">
+                    <div class="grid-x grid-padding-x grid-margin-x">
+                        <div class="small-12 medium-4 cell padding-top margin-top-xl primary-background process-box">
+                            <div class="number-outer">
+                                <span class="number-circle">1</span>
+                            </div>
+                            <h3 class = "center padding-top">Contact</h3>
+                            <hr>
+                            <p class = "center no-padding no-margin padding-bottom">You know us. Let us get to know you. The first step to being a difference maker in your church is reaching out.</p>
+                        </div>
+
+                        <div class="small-12 medium-4 cell padding-top margin-top-xl primary-background process-box">
+                            <div class="number-outer">
+                                <span class="number-circle">2</span>
+                            </div>
+                            <h3 class = "center padding-top">Discovery</h3>
+                            <hr>
+                            <p class = "center no-padding no-margin padding-bottom">Let us reach out to you. We want to discover your goals so we can grow your ministry.</p>
+                        </div>
+
+                        <div class="small-12 medium-4 cell padding-top margin-top-xl primary-background process-box">
+                            <div class="number-outer">
+                                <span class="number-circle">3</span>
+                            </div>
+                            <h3 class = "center padding-top">Proposal</h3>
+                            <hr>
+                            <p class = "center no-padding no-margin padding-bottom">After we create a plan, we'll pitch a proposal that not only fits in your budget, but crushes your goals.</p>
+                        </div>
+
+                        <div class="small-12 medium-4 medium-4 medium-offset-2 cell padding-top margin-top-xl primary-background process-box">
+                            <div class="number-outer">
+                                <span class="number-circle">4</span>
+                            </div>
+                            <h3 class = "center padding-top">Consulting</h3>
+                            <hr>
+                            <p class = "center no-padding no-margin padding-bottom">It's time to set the plan into action. We will meet with you and the consulting will begin!</p>
+                        </div>
+
+                        <div class="small-12 medium-4 cell padding-top margin-top-xl primary-background process-box">
+                            <div class="number-outer">
+                                <span class="number-circle">5</span>
+                            </div>
+                            <h3 class = "center padding-top">Recommendation</h3>
+                            <hr>
+                            <p class = "center no-padding no-margin padding-bottom">Now it's your turn! We'll give you a digital copy of your plan and you can impact your ministry!</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
 <?php get_footer();
