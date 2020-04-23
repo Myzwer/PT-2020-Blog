5d59122a518586fc4f3ab5e1bc287ba55826ed0e
Josh Forrester <twicearoundmars@yahoo.com>
2020-04-23 01:59:33 -0400

    Created an about us page template, and edited scss files to accompany the design.

---
+++ devwp/about.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Template Name: About Me
+ *
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ *
+ * @package WordPress
+ * @subpackage Caelicarn
+ * @since 1.0.0
+ */
+
+get_header(); ?>
+
+<div class="grid-container full-width">
+    <div class="grid-x grid-padding-x full-background full-background-xl" style = "background: url(http://pt-blog-2020.local/wp-content/uploads/2020/04/about-header.jpg);  background-position: top center;background-repeat: no-repeat;background-attachment: scroll; background-size: cover;">
+        <div class="small-12 cell">
+            <div class="content-middle width-large">
+                    <h1 class = "center" >about</h1>
+            </div>
+        </div>
+    </div>
+</div>
+
+    <div class="full-width main-background">
+        <div class = "grid-container padding-bottom padding-top">
+            <div class="grid-x grid-padding-x primary-background">
+                <div class="large-12 cell padding-top">
+                    <h2>"My desire is to serve churches with humility and authenticity to help connect more people to the Gospel."</h2>
+                    <p class = "no-margin no-padding padding-bottom">Dr. Trenton J. Stewart is the lead pastor of Foothills Church in Maryville, Tenn. The church is the heart and vision of Pastor Trent who planted FC in 2009 with the support of Grace Baptist Church in Knoxville and Pastor Trent’s father, Dr. Ron Stewart – the longtime lead pastor of GBC.</p>
+                    <p class = "no-margin no-padding padding-bottom">Since 2009, Pastor Trent’s vision and hope for FC derives from a deep passion to develop mature disciples of Christ in relational environments. Through the ministry of Foothills Church, he and the FC staff have seen phenominal growth and ministry take place in their city and even across the globe.</p>
+                    <p class = "no-margin no-padding padding-bottom">Today, Pastor Trent shares the hope of Jesus to over 2,000 people weekly as part of two Sunday services. God’s provision for the heart of FC has helped fuel it to become one of the fastest-growing churches in the area – growing in attendance by more than 20 percent from 2019-20. FC has been a sending church through The North American Mission Board to help plant churches in the U.S. FC has also sent missionaries to London, and Africa through International Mission Board. In 2018 the FC School of Ministry was developed to equip leaders and pastors to lead God's church.</p>
+                    <p class = "no-margin no-padding padding-bottom">In 2018, Pastor Trent and the FC staff unveiled the 2020 Vision for FC, which includes the vision to Multiply Campuses, Multiply Leaders, and Multiply Resources. Part of this vision is to plant a campus in the Knoxville area, build a counseling center on campus, and renovate the current building for kids and students.</p>
+                    <p class = "no-margin no-padding padding-bottom">Prior to planting FC, Pastor Trent attended Carson Newman College on a basketball scholarship and graduated with his Bachelor of Science in Business Administration in 1999. Pastor Trent holds a Master of Divinity and a Doctorate of Ministry from The Southern Baptist Theological Seminary.</p>
+                    <p class = "no-margin no-padding padding-bottom">Pastor Trent and his wife Micah have been married for 20 years and have four children: Baylee, Bryson, Brooke, and Blakely.</p>
+                    <div class="margin-bottom">
+                        <button class="center no-margin btn btn-v1">Contact Me</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class = "grid-container padding-bottom padding-top">
+            <div class="grid-x grid-padding-x primary-background">
+                <div class="large-12 cell padding-top padding-bottom">
+                    <img src="http://pt-blog-2020.local/wp-content/uploads/2020/04/Trent-Family-Photo-rectangle-1024x687-1.jpg" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="grid-container full-width">
+        <div class="grid-x grid-padding-x full-background " style = "background: linear-gradient(
+      rgba(0, 0, 0, 0.45),
+      rgba(0, 0, 0, 0.45)
+    ),url(http://pt-blog-2020.local/wp-content/uploads/2020/04/Watch-Page.jpg);   background-position: center center;background-repeat: no-repeat;background-attachment: scroll; background-size: cover;">
+            <div class="small-12 cell">
+                <div class="content-middle">
+                    <h1 class = "center" >Let's Be Social</h1>
+                    <div class = "text-invert socials center">
+                        <a href="#"><i class="fab fa-facebook"></i></a>
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                        <a href="#"><i class="fab fa-youtube"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+<?php get_footer();
