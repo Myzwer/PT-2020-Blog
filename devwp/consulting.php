baac40e561e0207b529337caad396cce8f42147a
Josh Forrester <twicearoundmars@yahoo.com>
2020-04-23 10:42:44 -0400

    Created new consultation page with header and minor body text.

---
+++ devwp/consulting.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Template Name: Consulting
+ *
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ *
+ * @package WordPress
+ * @subpackage Caelicarn
+ * @since 1.0.0
+ */
+
+get_header(); ?>
+
+    <div class="grid-container full-width">
+        <div class="grid-x grid-padding-x full-background full-background-xl" style = "background: url(http://pt-blog-2020.local/wp-content/uploads/2020/04/Consulting.jpg);  background-position: top center;background-repeat: no-repeat;background-attachment: scroll; background-size: cover;">
+            <div class="small-12 cell">
+                <div class="content-middle width-large">
+                    <h1 class = "center" >Consulting</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="full-width main-background">
+        <div class = "grid-container padding-bottom padding-top">
+            <div class="grid-x grid-padding-x primary-background">
+                <div class="large-12 cell padding-top">
+                    <h2>// Helping churches create healthy environments that grow healthy ministries</h2>
+                    <p class = "no-margin no-padding padding-bottom">Every church is unique and requires a tailored approach. With our detailed consultation process, our team aims to create a healthy state that will lead to a culture of multiplying disciples for God's Kingdom to advance. I love God's Church, and we desire to see disciples created for His glory. There is no secret to growth. However, we do know God will do his part. The question remains, 'Will you do yours?'</p>
+                    <div class="margin-bottom">
+                        <button class="center no-margin btn btn-v1">Reach Out Today</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+<?php get_footer();
