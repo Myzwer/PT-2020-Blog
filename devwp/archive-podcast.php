85db2fd9a5e02d4d4ff660e0eb2f8d0fe2af3678
Josh Forrester <twicearoundmars@yahoo.com>
2020-05-07 11:08:26 -0400

    Created the listen page, hard coded until client sees it.

--- devwp/archive-podcast.php
+++ devwp/archive-podcast.php
@@ -14,100 +14,74 @@ setup_postdata($post);
 
 
     <div class="grid-container full-width">
-        <div class="grid-x grid-padding-x full-background full-background-xl" style = "background: url(http://pt-blog-2020.local/wp-content/uploads/2020/05/Listen-Page.jpg);  background-position: top center;background-repeat: no-repeat;background-attachment: scroll; background-size: cover;">
-            <div class="small-12 cell">
-                <div class="content-middle width-large">
-                    <h1 class = "center" >Listen</h1>
+        <div class="grid-x grid-padding-x full-background" style = "background: linear-gradient(
+      rgba(0, 0, 0, 0.45),
+      rgba(0, 0, 0, 0.45)
+    ),url(http://pt-blog-2020.local/wp-content/uploads/2020/05/Listen-Page.jpg);  background-position: top center;">
+            <div class="large-12 cell">
+                <div class="content-middle">
+                    <h1 class = "center" >Latest Podcast</h1>
+                    <p class = "text-invert center podcast-title">Worship That Changes The World</p>
+                    <button class="btn btn-v2 center">Listen Now</button>
                 </div>
             </div>
         </div>
     </div>
 
 
+    <div class="full-width primary-background padding-bottom">
+        <div class = "grid-container padding-bottom padding-top">
+            <div class="grid-x grid-padding-x main-background rounded-corners">
+                <div class="small-12 cell padding-top">
+                    <h2 class = "center ">// The Ascent Podcast</h2>
+                    <p class = " center padding-bottom">Listen along as Pastor Trent talks about stuff that is relevant to other things</p>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <div class="full-width main-background">
         <div class = "grid-container padding-bottom padding-top">
-            <div class="grid-x grid-padding-x primary-background">
+            <div class="grid-x grid-margin-x">
+                <div class="small-12 medium-6 cell podcast-card margin-bottom">
+                    <a href="#">
+                        <img src="https://trentstewart.org/wp-content/uploads/2019/10/Podcast-11.jpg" alt="">
+                    </a>
+                    <div class="add-padding">
+                        <a href = "#">
+                            <h2 class = "capitalize">Worship That Changes The World</h2>
+                        </a>
+                        <p class = "no-margin no-padding">From The Ascent Podcast</p>
+                        <p class = "no-margin no-padding">October 30, 2019</p>
+                    </div>
+                </div>
 
+                <div class="small-12 medium-6 cell podcast-card margin-bottom">
+                    <a href="#">
+                        <img src="https://trentstewart.org/wp-content/uploads/2019/10/Podcast-10.jpg" alt="">
+                    </a>
+                    <div class="add-padding">
+                        <a href = "#">
+                            <h2 class = "capitalize">How To Influence Culture</h2>
+                        </a>
+                        <p class = "no-margin no-padding">From The Ascent Podcast</p>
+                        <p class = "no-margin no-padding">October 16, 2019</p>
+                    </div>
+                </div>
 
-            <?php
-
-            // WP_Query arguments
-            $args = array(
-                'post_type' => array('broadcast'),
-                'post_status' => array('publish'),
-                'nopaging' => false,
-                'order' => 'DESC',
-                'orderby' => 'date',
-                'paged' => (get_query_var('paged')) ? get_query_var('paged') : 1,
-                'tax_query' => array(
-                    array(
-                        'taxonomy' => 'format',
-                        'terms' => 'podcasts',
-                        'field' => 'slug'
-                    )
-                )
-            );
-
-            // The Query
-            $broadcasts = new WP_Query($args);
-
-            // The Loop
-            if ($broadcasts->have_posts()) {
-                while ($broadcasts->have_posts()) {
-                    $broadcasts->the_post();
-
-                    echo "<div class='small-12 medium-6 large-4 cell'>";
-                    echo "<div class = 'podcast-post'>";
-                    the_post_thumbnail();
-
-                    echo '<h2 class = "title"><a href="' . get_field('podcast_link') . '">' . get_the_title() . '</a></h2>';
-
-
-                    echo "<span class='author'>";
-//                    echo 'featuring ';
-                    the_author();
-                    echo "</span>";
-                    echo " | ";
-
-                    echo "<span class='date'>";
-                    echo get_the_date();
-                    echo "</span>";
-
-                    echo "<div class='blog-excerpt'>";
-                    the_excerpt('<p class = "blog-excerpt">', '</p>');
-
-                    echo "</div>";
-                    echo "</div>";
-                    echo "</div>";
-
-                }
-                echo "<div class='page-nav'>";
-                echo "<div class='navigation'>";
-                echo "<ul>";
-                echo paginate_links(array(
-                    'base' => str_replace(999999999, '%#%', esc_url(get_pagenum_link(999999999))),
-                    'total' => $broadcasts->max_num_pages,
-                    'current' => max(1, get_query_var('paged')),
-                    'format' => '?paged=%#%',
-                    'show_all' => false,
-                    'type' => 'list',
-                    'end_size' => 2,
-                    'mid_size' => 1,
-                    'prev_next' => true,
-                    'prev_text' => sprintf('<i></i> %1$s', __('Newer Posts', 'text-domain')),
-                    'next_text' => sprintf('%1$s <i></i>', __('Older Posts', 'text-domain')),
-                    'add_args' => false,
-                    'add_fragment' => '',
-                ));
-                echo "</ul>";
-                echo "</div>";
-                echo "</div>";
-            } else {
-                echo 'there are no posts.'; // no posts found
-            }
-
-            ?>
+                <div class="small-12 medium-6 cell podcast-card margin-bottom">
+                    <a href="#">
+                        <img src="https://trentstewart.org/wp-content/uploads/2019/10/Podcast-5.jpg" alt="">
+                    </a>
+                    <div class="add-padding">
+                        <a href = "#">
+                            <h2 class = "capitalize">Beginning With The End In Mind</h2>
+                        </a>
+                        <p class = "no-margin no-padding">From The Ascent Podcast</p>
+                        <p class = "no-margin no-padding">October 2, 2019</p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
