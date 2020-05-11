51ac3febcffec5ab79033d85d0164f94036b80f5
Josh Forrester <twicearoundmars@yahoo.com>
2020-05-11 12:30:49 -0400

    Added broadcast page to display sermons. Once again, this is hard coded until design gets approval

---
+++ devwp/archive-broadcast.php
@@ -0,0 +1,103 @@
+<?php
+
+/**
+ * Template Name: Sermon Archives
+ **/
+
+get_header();
+
+
+$this_page = get_page_by_path('broadcasts');
+$post = $this_page;
+setup_postdata($post);
+?>
+
+
+    <div class="grid-container full-width">
+        <div class="grid-x grid-padding-x full-background" style = "background: linear-gradient(
+      rgba(0, 0, 0, 0.45),
+      rgba(0, 0, 0, 0.45)
+    ),url(http://pt-blog-2020.local/wp-content/uploads/2020/05/Watch-Header.jpg);  background-position: top center;">
+            <div class="large-12 cell">
+                <div class="content-middle">
+                    <h1 class = "center" >Latest Video</h1>
+                    <p class = "text-invert center podcast-title">No Other Gospel</p>
+                    <button class="btn btn-v2 center">Watch Now</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="full-width primary-background padding-bottom">
+        <div class = "grid-container padding-bottom padding-top">
+            <div class="grid-x grid-padding-x main-background rounded-corners">
+                <div class="small-12 cell padding-top">
+                    <h2 class = "center ">Watch</h2>
+                    <p class = " center padding-bottom">Idk man watch stuff here or whatever.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="full-width main-background">
+        <div class = "grid-container padding-bottom padding-top">
+            <div class="grid-x grid-margin-x">
+
+                <div class="small-12 medium-6 medium-offset-6 cell margin-bottom">
+                    <div class = "filter-box">Filter by:
+                        <p class = " category-box">All</p>
+                        <p class = " category-box">Sermons</p>
+                        <p class = " category-box">Development</p>
+                        <p class = " category-box">Updates</p>
+                    </div>
+                </div>
+
+                <div class="small-12 medium-6 cell podcast-card margin-bottom">
+                    <a href="#">
+                        <img src="http://pt-blog-2020.local/wp-content/uploads/2020/04/PT-About-Section.jpg" alt="">
+                    </a>
+                    <div class="add-padding">
+                        <a href = "#">
+                            <h2 class = "capitalize">No Other Gospel</h2>
+                        </a>
+                        <p class = "no-margin no-padding">May 3, 2020</p>
+                        <p class = "no-margin no-padding category-box">Sermon</p>
+                    </div>
+                </div>
+
+                <div class="small-12 medium-6 cell podcast-card margin-bottom">
+                    <a href="#">
+                        <img src="https://b16b64e9fc79863eb097-2d57ced7297adfa11a0f9d9e6bf0a0a1.ssl.cf2.rackcdn.com/uploaded/c/0e10006853_1584821214_coronavirus-updates.jpg" alt="">
+                    </a>
+                    <div class="add-padding">
+                        <a href = "#">
+                            <h2 class = "capitalize">COVID-19 Update #4</h2>
+                        </a>
+                        <p class = "no-margin no-padding">May 1, 2020</p>
+                        <p class = "no-margin no-padding category-box">Updates</p>
+                    </div>
+                </div>
+
+                <div class="small-12 medium-6 cell podcast-card margin-bottom">
+                    <a href="#">
+                        <img src="http://pt-blog-2020.local/wp-content/uploads/2020/04/PT-About-Section.jpg" alt="">
+                    </a>
+                    <div class="add-padding">
+                        <a href = "#">
+                            <h2 class = "capitalize">How to worship during the Ronas</h2>
+                        </a>
+                        <p class = "no-margin no-padding">April 15, 2020</p>
+                        <p class = "no-margin no-padding category-box">Development</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+<?php
+
+
+get_footer();
+?>
