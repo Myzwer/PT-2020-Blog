4f6dff62b22e28538fa44ddd27aacfba70dfa52f
Myzwer <josh@onefortyfivedesign.com>
2020-06-25 13:02:10 -0400

    Updated the front-page.php to use an image background on mobile as opposed to forcing users to load in a video on their phone.

--- devwp/front-page.php
+++ devwp/front-page.php
@@ -13,15 +13,45 @@
 
 get_header(); ?>
 
-    <video src="<?php the_field('video_background'); ?>" autoplay loop playsinline muted></video>
-
-    <div class="viewport-header">
-        <div class="head-container">
-            <div class="headline"><h1><?php the_field('page_title'); ?></h1></div>
-            <div class="knockout-logo"><img src="<?php the_field('logo'); ?>"></div>
+<!--Desktop code to display video on tablet and up.-->
+    <div class="show-for-desktop">
+        <video src="<?php the_field('video_background'); ?>" autoplay loop playsinline muted></video>
+        <div class="viewport-header">
+            <div class="head-container">
+                <div class="headline"><h1><?php the_field('page_title'); ?></h1></div>
+                <div class="knockout-logo"><img src="<?php the_field('logo'); ?>"></div>
+            </div>
         </div>
     </div>
 
+<!--Mobile code to remove video and replace with a static image -->
+    <div class="show-for-mobile">
+        <?php if( have_rows('header_image_settings') ): ?>
+            <?php while( have_rows('header_image_settings') ): the_row(); ?>
+                <div class="grid-container full-width">
+                    <div class="grid-x grid-padding-x full-background <?php the_sub_field('header_size'); ?>" style = "background: linear-gradient(
+                            rgba(0, 0, 0, <?php the_sub_field('transparency_filter'); ?>),
+                            rgba(0, 0, 0, <?php the_sub_field('transparency_filter'); ?>)
+                            ),url(<?php the_sub_field('header_background'); ?>);
+                            background-position: <?php the_sub_field('image_alignment'); ?> center;
+                            background-repeat: no-repeat;
+                            background-attachment: scroll;
+                            background-size: cover;">
+                        <div class="small-12 cell">
+                            <div class="content-middle width-large">
+                                <div class="viewport-header">
+                                    <div class="head-container">
+                                    <h1 class = "center" ><?php the_field('page_title'); ?></h1>
+                                    <div class="knockout-logo"><img src="<?php the_field('logo'); ?>">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <?php endwhile; ?>
+        <?php endif; ?>
+    </div>
 
 
 <div class="full-width main-background">
