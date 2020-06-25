34f92d877a05a3edd2933083f23572ca8584be80
Myzwer <josh@onefortyfivedesign.com>
2020-06-25 16:22:13 -0400

    Updated the about template name from about me to about.

--- devwp/about.php
+++ devwp/about.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: About Me
+ * Template Name: About
  *
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
@@ -39,11 +39,11 @@ get_header(); ?>
             <div class="grid-x grid-padding-x primary-background">
                 <div class="large-12 cell padding-top">
                     <h3 class = "padding-bottom center">"My desire is to serve churches with humility and authenticity to help connect more people to the Gospel."</h3>
-                    <p class = "no-margin no-padding padding-bottom">Dr. Trenton J. Stewart is the lead pastor of Foothills Church in Maryville, Tenn. The church is the heart and vision of Pastor Trent who planted FC in 2009 with the support of Grace Baptist Church in Knoxville and Pastor Trent’s father, Dr. Ron Stewart – the longtime lead pastor of GBC.
+                    <p class = "no-margin ">Dr. Trenton J. Stewart is the lead pastor of Foothills Church in Maryville, Tenn. The church is the heart and vision of Pastor Trent who planted FC in 2009 with the support of Grace Baptist Church in Knoxville and Pastor Trent’s father, Dr. Ron Stewart – the longtime lead pastor of GBC.
                     Since 2009, Pastor Trent’s vision and hope for FC derives from a deep passion to develop mature disciples of Christ in relational environments. Through the ministry of Foothills Church, he and the FC staff have seen phenominal growth and ministry take place in their city and even across the globe.</p>
-                    <p class = "no-margin no-padding padding-bottom">Today, Pastor Trent shares the hope of Jesus to over 2,000 people weekly as part of two Sunday services. God’s provision for the heart of FC has helped fuel it to become one of the fastest-growing churches in the area – growing in attendance by more than 20 percent from 2019-20. FC has been a sending church through The North American Mission Board to help plant churches in the U.S. FC has also sent missionaries to London, and Africa through International Mission Board. In 2018 the FC School of Ministry was developed to equip leaders and pastors to lead God's church.
+                    <p class = "no-margin ">Today, Pastor Trent shares the hope of Jesus to over 2,000 people weekly as part of two Sunday services. God’s provision for the heart of FC has helped fuel it to become one of the fastest-growing churches in the area – growing in attendance by more than 20 percent from 2019-20. FC has been a sending church through The North American Mission Board to help plant churches in the U.S. FC has also sent missionaries to London, and Africa through International Mission Board. In 2018 the FC School of Ministry was developed to equip leaders and pastors to lead God's church.
                     In 2018, Pastor Trent and the FC staff unveiled the 2020 Vision for FC, which includes the vision to Multiply Campuses, Multiply Leaders, and Multiply Resources. Part of this vision is to plant a campus in the Knoxville area, build a counseling center on campus, and renovate the current building for kids and students.</p>
-                    <p class = "no-margin no-padding padding-bottom">Prior to planting FC, Pastor Trent attended Carson Newman College on a basketball scholarship and graduated with his Bachelor of Science in Business Administration in 1999. Pastor Trent holds a Master of Divinity and a Doctorate of Ministry from The Southern Baptist Theological Seminary.
+                    <p class = "no-margin ">Prior to planting FC, Pastor Trent attended Carson Newman College on a basketball scholarship and graduated with his Bachelor of Science in Business Administration in 1999. Pastor Trent holds a Master of Divinity and a Doctorate of Ministry from The Southern Baptist Theological Seminary.
                     Pastor Trent and his wife Micah have been married for 20 years and have four children: Baylee, Bryson, Brooke, and Blakely.</p>
                     <div class="margin-bottom">
                         <a href="/contact">
@@ -68,8 +68,8 @@ get_header(); ?>
       rgba(0, 0, 0, 0.45)
     ),url(http://pt-blog.local/wp-content/uploads/2020/04/Watch-Page.jpg);   background-position: center center;background-repeat: no-repeat;background-attachment: scroll; background-size: cover;">
             <div class="small-12 cell">
-                <div class="content-middle">
-                    <h1 class = "center" >Let's Be Social</h1>
+                <div class="content-middle full-width">
+                    <h1 class = "center mobile-heading-small" >Let's Be Social</h1>
                     <div class = "text-invert socials center">
                         <a href="https://www.facebook.com/trentonjstewart" target="_blank"><i class="fab fa-facebook text-invert"></i></a>
                         <a href="https://twitter.com/trentonjstewart?lang=en" target="_blank"><i class="fab fa-twitter text-invert"></i></a>
