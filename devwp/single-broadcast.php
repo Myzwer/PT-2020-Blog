78fc5404c955a2841ebbbbd01725baec53bf65d2
Myzwer <josh@onefortyfivedesign.com>
2020-07-29 18:50:06 -0400

    Both videos and podcasts use the same single-broadcast.php page, so I set up some php to switch the text and links of the buttons to be dynamic between video and podcast.

--- devwp/single-broadcast.php
+++ devwp/single-broadcast.php
@@ -51,26 +51,39 @@ get_header();
     <div class="full-width main-background">
         <div class = "grid-container padding-bottom">
             <div class="grid-x grid-padding-x primary-background add-padding">
+                <?php
+                $terms = get_the_terms($post, 'format');
+                $formatName = "";
+                $formatNameSingle = "";
+                foreach ($terms as $term) {
+                $formatName = " " . $term->name; // will result in " Podcasts"
+                $formatNameSingle = " " . rtrim($formatName, "s"); // will result in " Podcast"
+                break;
+                }
+                ?>
                 <div class="large-4 cell margin-bottom margin-top">
                     <a href="">
-                        <?php next_post_link('%link', '<button class="btn btn-v1 center">Next Video</button>', true, "", 'format'); ?>
+                        <?php next_post_link('%link', '<button class="btn btn-v1 center">Next' . $formatNameSingle . '</button>', true, "", 'format'); ?>
                     </a>
                 </div>
                 <div class="large-4 cell margin-bottom margin-top">
-                    <a href="/watch">
-                        <button class="btn btn-v1 center">All Videos</button>
+                    <a href="/<?php
+                    if ( get_field('video_link') ) { //check to see if the video ACF field is being used
+                        echo "watch";
+                    } else {
+                        echo "listen";
+                    } ?>">
+                        <!-- If $formatName is empty, then it will just say "All". Otherwise it would say "All Podcasts" -->
+                        <button class="btn btn-v1 center"><?php echo "All" . $formatName ?></button>
                     </a>
                 </div>
                 <div class="large-4 cell margin-bottom margin-top">
-                    <?php previous_post_link('%link', '<button class="btn btn-v1 center">Previous Video</button>', true, "", 'format'); ?>
+                    <?php previous_post_link('%link', '<button class="btn btn-v1 center">Previous' . $formatNameSingle . '</button>', true, "", 'format'); ?>
                 </div>
 
             </div>
         </div>
     </div>
-
-
-
 
 
 
