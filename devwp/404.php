398135fab30ead581b2530edda47486c8fe555f1
Myzwer <josh@onefortyfivedesign.com>
2020-09-15 12:22:07 -0400

    Updated some placement of inactive buttons on the 404 page and made them active.

--- devwp/404.php
+++ devwp/404.php
@@ -17,7 +17,7 @@ get_header(); ?>
             <div class="grid-x grid-padding-x full-background full-background-xl" style = "background: linear-gradient(
                 rgba(0, 0, 0, .5),
                 rgba(0, 0, 0, .5)
-                ),url(http://pt-blog.local/wp-content/uploads/2020/06/404.jpg);
+                ),url(http://pt-blog.local/wp-content/uploads/2020/07/03-29-20-Sermon.jpg);
                 background-position: top center;
                 background-repeat: no-repeat;
                 background-attachment: scroll;
@@ -27,11 +27,18 @@ get_header(); ?>
                         <h1 class = "center" >God Moves in Mysterious Ways</h1>
                         <h2 class = "center text-invert" >But this website shouldn't. You've found a broken link.</h2>
                         <h3 class = "center text-invert" >Let's get you where you were going.</h3>
-                        <div class="margin-bottom">
-                            <button class="center no-margin btn btn-v2">Back to the Homapage</button>
-                        </div>
-                        <div class="margin-bottom">
-                            <button class="center no-margin btn btn-v2">Watch A Video!</button>
+                        <div class="center margin-top">
+                            <span class="margin-bottom">
+                                <a href="/frontpage">
+                                    <button class="center no-margin btn btn-v2">Back to the Homapage</button>
+                                </a>
+                            </span>
+                            
+                            <span class="margin-bottom">
+                                <a href="/watch">
+                                    <button class="center no-margin btn btn-v2">Watch A Video!</button>
+                                </a>
+                            </span>
                         </div>
                     </div>
                 </div>
